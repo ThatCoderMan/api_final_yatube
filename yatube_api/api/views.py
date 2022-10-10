b2ddd4ed11c1e96e0fb07c6307ddf7323a3c45a1
Артемий Березин <artemii@MacBook-Air-Artemij.local>
2022-10-10 23:29:53 +0300

    1

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -8,7 +8,9 @@ from rest_framework.permissions import IsAuthenticated
 from posts.models import Follow, Group, Post
 
 from .permissions import IsAuthorOrReadOnly
-from .serializers import (CommentSerializer, FollowSerializer, GroupSerializer,
+from .serializers import (CommentSerializer, FollowSerializer,
+                          GroupSerializer,
+
                           PostSerializer)
 
 
